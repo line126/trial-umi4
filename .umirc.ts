5c0de851db8beada9c4286049b01b7f0cef6e91f
line126 <line112266@126.com>
2022-11-03 01:26:20 +0800

    chore: Configure

--- .umirc.ts
+++ .umirc.ts
@@ -1,8 +1,10 @@
-export default {
+import { defineConfig } from 'umi';
+
+export default defineConfig({
   npmClient: 'pnpm',
   plugins: ['@umijs/plugins/dist/antd', './umi-plugins/plugin-demo'],
   antd: {
     // dark: true,
     import: true, // babel-plugin-import
   },
-};
+});
